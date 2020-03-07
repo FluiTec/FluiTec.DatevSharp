a65f97287668380660833cc3af71d40a5768c583
Achim Schnell <a.schnell@wtschnell.de>
2020-03-07 11:39:34 +0100

    Now using space instead of string.Empty to replace new-line chars, also escape quotes.

--- src/FluiTec.DatevSharp/Helpers/StringHelper.cs
+++ src/FluiTec.DatevSharp/Helpers/StringHelper.cs
@@ -1,18 +1,30 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace FluiTec.DatevSharp.Helpers
 {
 	/// <summary>   A string helper. </summary>
 	public static class StringHelper
 	{
-		/// <summary>   A string extension method that converts a str to a datev string. </summary>
-		///
-		/// <param name="str">  The str to act on. </param>
-		///
-		/// <returns>   str as a string. </returns>
-		public static string ToDatev(this string str)
-		{
-			return string.IsNullOrWhiteSpace(str) ? "\"\"" : Regex.Replace($"\"{str}\"", @"\r\n?|\n", "");
-		}
-	}
+        private static readonly Dictionary<string, string> ReplaceMap = new Dictionary<string, string>
+        {
+            {"\r\n", " " },
+            {"\n", " " },
+            {"\"",  "\"\""}
+        };
+
+        private static readonly string Regex = string.Join("|", ReplaceMap.Keys);
+
+        /// <summary>   A string extension method that converts a str to a datev string. </summary>
+        ///
+        /// <param name="str">  The str to act on. </param>
+        ///
+        /// <returns>   str as a string. </returns>
+        public static string ToDatev(this string str)
+        {
+            return string.IsNullOrWhiteSpace(str)
+                ? "\"\""
+                : "\"" + System.Text.RegularExpressions.Regex.Replace(str, Regex, m => ReplaceMap[m.Value]) + "\"";
+        }
+    }
 }
